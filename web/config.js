7aad5fbaafdafa6f9b2dad7bccc00f6cec001076
feyi-tech <jinminetics@gmail.com>
2025-08-03 09:04:23 +0100

    web firebase config vars added

--- web/config.js
+++ web/config.js
@@ -6,6 +6,7 @@ const config = {
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
   },
   turnstile: {
     siteKey: process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY,
